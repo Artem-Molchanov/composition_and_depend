d7b819633f039a50dff33573908a8bc1db477e69
Artem Molchanov <artem700@gmail.com>
2023-05-03 15:56:09 -0300

    improved findLast

--- src/main/java/ru/netology/MovieManager.java
+++ src/main/java/ru/netology/MovieManager.java
@@ -17,7 +17,7 @@ public class MovieManager {
 
     public void add(String movie) {
 
-        String[] tmp = new String[movies.length + 1];
+        String[] tmp = new String[movies.length +1];
         for (int i = 0; i < movies.length; i++) {
             tmp[i] = movies[i];
         }
@@ -40,7 +40,7 @@ public class MovieManager {
             resultLength = limit;
         }
         String[] tmp = new String[resultLength];
-        for (int i = 0; i < movies.length; i++) {
+        for (int i = 0; i < resultLength; i++) {
             tmp[i] = movies[movies.length - 1 - i];
         }
         return tmp;
